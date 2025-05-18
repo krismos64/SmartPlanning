a02d047d61dfeda5018d9817c0fc648a59d8212a
krismos64 <c.mostefaoui@yahoo.fr>
2025-05-18 08:30:50 +0200

    fix axios instance

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -7,8 +7,10 @@
 
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5050/api";
+
 const axiosInstance = axios.create({
-  baseURL: "/",
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
