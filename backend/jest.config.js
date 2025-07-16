7eedaf13aeafd923f57d2e3aa1a2196abb26ee6f
krismos64 <c.mostefaoui@yahoo.fr>
2025-07-16 23:58:16 +0200

    update backend tsconfig

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,28 +1,29 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: [
-    '**/__tests__/**/*.test.ts',
-    '**/?(*.)+(spec|test).ts'
-  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/scripts/**',
-    '!src/index.ts',
-    '!src/server.ts'
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/scripts/**",
+    "!src/index.ts",
+    "!src/server.ts",
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
   testTimeout: 30000,
   verbose: true,
   detectOpenHandles: true,
   forceExit: true,
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: false
-    }]
-  }
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: false,
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
 };
